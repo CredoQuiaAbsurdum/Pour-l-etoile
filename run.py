7ee9cf6cab861716b4c3e36ff77b16a350db9cc9
CredoQuiaAbsurdum <virta_kuivapuu@hotmail.com>
2018-07-17 00:02:16 -0300

    Update list12

--- run.py
+++ run.py
@@ -6,31 +6,8 @@ import sys
 
 
 if __name__ == '__main__':
-    # Generate shuffled vocabulary
-    if (len(sys.argv)>1):
-        # Generate shuffled vocabulary
-        if(sys.argv[1]=='-shuffle'):
-            if (len(sys.argv)==2):
-                LIST.shuffle()
-            else:
-                seed = ' '.join(map(str, sys.argv[2:]))
-                LIST.shuffle(seed)
-            filename = 'version_%s.txt' % str(LIST.seed)
-            file = open(filename, 'w+')
-            file.write(str(LIST))
-
-        # Search a word
-        if(sys.argv[1]=='-search'):
-            try:
-                if (len(sys.argv)>2):
-                    word = LIST.search(str(sys.argv[2]))
-                    print (word)
-                else:
-                    print ("请输入词条")
-            except:
-                print ("词条 %s 不合法" % sys.argv[2])
-
-    #LIST.shuffle('Francis')
-
+    filename = 'version_%s.txt' % str(LIST.version)
+    file = open(filename, 'w+')
+    file.write(str(LIST))
 
 
